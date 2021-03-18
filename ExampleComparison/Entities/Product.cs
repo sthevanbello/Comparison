f59c80f394a8268cbf40ffd58b6453f96e3ecc83
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-18 17:13:39 -0300

    Example using List with IComparable

---
+++ ExampleComparison/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleComparison.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double myProPriceperty)
+        {
+            Name = name;
+            Price = myProPriceperty;
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+
+        public override string ToString()
+        {
+            return $"name: {Name}    \tPrice: {Price:C} ";
+        }
+    }
+}
---
+++ ExampleComparison/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExampleComparison.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ExampleComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Television", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+            list.Add(new Product("SmartPhone", 1000.00));
+
+            list.Sort();
+
+            foreach (Product item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
